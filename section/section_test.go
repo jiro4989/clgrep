10f2f4beb171aa2e4afe44e3bff0b0eef0aa0370
jiro4989 <jiroron666@gmail.com>
2018-08-21 19:33:03 +0900

    テストコード追加

--- section/section_test.go
+++ section/section_test.go
@@ -7,51 +7,133 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var sect = func(s ...string) Section { return s }
+var sects = func(s ...Section) Sections { return s }
+
 func TestMatchesRegexp(t *testing.T) {
-	f := func(s ...string) Section { return s }
 	re := regexp.MustCompile(`abc`)
 
-	assert.Equal(t, true, f("*test: foo", "abc").MatchesRegexp(re))
-	assert.Equal(t, true, f("*test: foo", "	abc").MatchesRegexp(re))
-	assert.Equal(t, true, f("*test: foo", "xabcx").MatchesRegexp(re))
-	assert.Equal(t, true, f("	*test: foo", "	xabcx").MatchesRegexp(re))
-	assert.Equal(t, false, f("foobar").MatchesRegexp(re))
-	assert.Equal(t, false, f().MatchesRegexp(re))
+	assert.Equal(t, true, sect("*test: foo", "abc").MatchesRegexp(re))
+	assert.Equal(t, true, sect("*test: foo", "	abc").MatchesRegexp(re))
+	assert.Equal(t, true, sect("*test: foo", "xabcx").MatchesRegexp(re))
+	assert.Equal(t, true, sect("	*test: foo", "	xabcx").MatchesRegexp(re))
+	assert.Equal(t, false, sect("foobar").MatchesRegexp(re))
+	assert.Equal(t, false, sect().MatchesRegexp(re))
 
 	re = regexp.MustCompile(``)
-	assert.Equal(t, true, f("*test: foo", "abc").MatchesRegexp(re))
+	assert.Equal(t, true, sect("*test: foo", "abc").MatchesRegexp(re))
 
 	re = regexp.MustCompile(`f..`)
-	assert.Equal(t, true, f("*test: foo", "abc").MatchesRegexp(re))
+	assert.Equal(t, true, sect("*test: foo", "abc").MatchesRegexp(re))
 }
 
 func TestMatchesTag(t *testing.T) {
-	f := func(s ...string) Section { return s }
 	re := regexp.MustCompile(`main`)
 
-	assert.Equal(t, true, f("*main.go: foobar", "abc").MatchesTag(re))
-	assert.Equal(t, true, f("*main.go: foobar").MatchesTag(re))
-	assert.Equal(t, true, f("*main.go: ").MatchesTag(re))
-	assert.Equal(t, true, f("*main.go:").MatchesTag(re))
-	assert.Equal(t, true, f("	*main.go:").MatchesTag(re))
-	assert.Equal(t, false, f("*hoge.go: main", "abc").MatchesTag(re))
-	assert.Equal(t, false, f("*hoge.go: main", "main").MatchesTag(re))
-	assert.Equal(t, false, f("*hoge.go: fuga", "main").MatchesTag(re))
-	assert.Equal(t, false, f("*main.go fuga", "main").MatchesTag(re))
-	assert.Equal(t, false, f("main.go: fuga", "main").MatchesTag(re))
-	assert.Equal(t, false, f("main.go fuga", "main").MatchesTag(re))
-	assert.Equal(t, false, f("main.go fuga", "*main.go: foobar", "main").MatchesTag(re))
-	assert.Equal(t, false, f().MatchesTag(re))
+	assert.Equal(t, true, sect("*main.go: foobar", "abc").MatchesTag(re))
+	assert.Equal(t, true, sect("*main.go: foobar").MatchesTag(re))
+	assert.Equal(t, true, sect("*main.go: ").MatchesTag(re))
+	assert.Equal(t, true, sect("*main.go:").MatchesTag(re))
+	assert.Equal(t, true, sect("	*main.go:").MatchesTag(re))
+	assert.Equal(t, false, sect("*hoge.go: main", "abc").MatchesTag(re))
+	assert.Equal(t, false, sect("*hoge.go: main", "main").MatchesTag(re))
+	assert.Equal(t, false, sect("*hoge.go: fuga", "main").MatchesTag(re))
+	assert.Equal(t, false, sect("*main.go fuga", "main").MatchesTag(re))
+	assert.Equal(t, false, sect("main.go: fuga", "main").MatchesTag(re))
+	assert.Equal(t, false, sect("main.go fuga", "main").MatchesTag(re))
+	assert.Equal(t, false, sect("main.go fuga", "*main.go: foobar", "main").MatchesTag(re))
+	assert.Equal(t, false, sect().MatchesTag(re))
 
 	re = regexp.MustCompile(`m...`)
-	assert.Equal(t, true, f("*main.go:").MatchesTag(re))
+	assert.Equal(t, true, sect("*main.go:").MatchesTag(re))
+}
+
+type TestMatchTagData struct {
+	expect Sections
+	in     Sections
 }
 
 func TestMatchTag(t *testing.T) {
-	// TODO
+	tds := []TestMatchTagData{
+		TestMatchTagData{
+			expect: sects(
+				sect("*main.go: foobar", "abc"),
+				sect("*main.go: hogebar", "abc"),
+				sect("*main.go: foobar", "abc"),
+			),
+			in: sects(
+				sect("*main.go: foobar", "abc"),
+				sect("*main.go: hogebar", "abc"),
+				sect("*main.go: foobar", "abc"),
+			),
+		},
+		TestMatchTagData{
+			expect: Sections{},
+			in: sects(
+				sect("*foobar.go: hogebar", "abc"),
+				sect("*MAIN.go: hogebar", "abc"),
+				sect("*foobar.go: hogebar", "abc"),
+			),
+		},
+		TestMatchTagData{
+			expect: Sections{},
+			in: sects(
+				sect("*foobar.go: main", "abc"),
+				sect("*MAIN.go: main", "abc"),
+				sect("*foobar.go: main", "abc"),
+			),
+		},
+		TestMatchTagData{
+			expect: Sections{},
+			in:     Sections{},
+		},
+	}
+	re := regexp.MustCompile(`main`)
+	for _, v := range tds {
+		assert.Equal(t, v.expect, v.in.MatchTag(re))
+	}
 }
 
 func TestReverse(t *testing.T) {
+	tds := []TestMatchTagData{
+		TestMatchTagData{
+			expect: sects(
+				sect("*main.go: 3", "6"),
+				sect("*main.go: 2", "5"),
+				sect("*main.go: 1", "4"),
+			),
+			in: sects(
+				sect("*main.go: 1", "4"),
+				sect("*main.go: 2", "5"),
+				sect("*main.go: 3", "6"),
+			),
+		},
+		TestMatchTagData{
+			expect: sects(
+				sect("*main.go: 2", "5"),
+				sect("*main.go: 1", "4"),
+			),
+			in: sects(
+				sect("*main.go: 1", "4"),
+				sect("*main.go: 2", "5"),
+			),
+		},
+		TestMatchTagData{
+			expect: sects(sect("*main.go: 1", "4")),
+			in:     sects(sect("*main.go: 1", "4")),
+		},
+		TestMatchTagData{
+			expect: Sections{},
+			in:     Sections{},
+		},
+	}
+	for _, v := range tds {
+		v.in.Reverse()
+		assert.Equal(t, v.expect, v.in)
+	}
+}
+
+func TestTrimSpace(t *testing.T) {
 	// TODO
 }
 
